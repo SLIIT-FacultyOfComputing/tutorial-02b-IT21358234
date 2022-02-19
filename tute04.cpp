ccfbc2671af1ee190366716d6d930b3a9db373f9
IT21358234 <99161169+IT21358234@users.noreply.github.com>
2022-02-19 05:36:40 +0000

    what did you change

--- tute04.cpp
+++ tute04.cpp
@@ -11,7 +11,7 @@ long Factorial(int no);
 long nCr(int n, int r);
 Do not modify the main function.*/
 
-#include <iostream>
+/*#include <iostream>
 
 long Factorial(int no);
 long nCr(int n, int r);
@@ -26,4 +26,36 @@ int main() {
   std::cout << nCr(n,r);
   std::cout << std::endl;
   return 0;
+}*/
+
+#include <iostream>
+long Factorial(int no);
+long nCr(int n, int r);
+int main() 
+{
+	
+	int n, r;
+	std::cout << "Enter a value for n ";
+	std::cin >> n;
+	std::cout << "Enter a value for r ";
+	std::cin >> r;
+ 	std::cout << "nCr = ";
+	std::cout << nCr(n,r);
+	std::cout << std::endl;
+}
+long Factorial(int no)
+{
+	int fac=1;
+	for(int i=1;i<=no;i++)
+	{
+		fac=fac*i;
+	}
+	
+	return fac;
+}
+long nCr(int n, int r)
+{
+	int nCr;
+	nCr = Factorial(n)/(Factorial(r)*Factorial(n-r));
+	return nCr;
 }
